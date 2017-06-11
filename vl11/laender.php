124ad14dc1f7ab115a43863f8df3679ffdb919c3
Wolfgang Hochleitner <wolfgang@hochleitner.cc>
2017-06-11 03:36:44 +0200

    Updated examples for lecture 11
    New jQuery and jQuery UI versions, cleaner XHR code, let vs var, updated
    SOP example to work with jQuery 3+.

--- vl11/laender.php
+++ vl11/laender.php
@@ -1,13 +1,15 @@
 <?php
-$staedte = ["Bitte ein Bundesland auswählen",
-            "Eisenstadt",
-            "Klagenfurt",
-            "Sankt Pölten",
-            "Linz",
-            "Salzburg",
-            "Graz",
-            "Innsbruck",
-            "Bregenz",
-            "Wien"];
+$staedte = [
+    "Bitte ein Bundesland auswählen",
+    "Eisenstadt",
+    "Klagenfurt",
+    "Sankt Pölten",
+    "Linz",
+    "Salzburg",
+    "Graz",
+    "Innsbruck",
+    "Bregenz",
+    "Wien"
+];
 
 echo $staedte[$_GET["index"]];
--- vl11/xmlhttprequest_ajax.js
+++ vl11/xmlhttprequest_ajax.js
@@ -1,42 +1,20 @@
-// Workaround für IE 5, 5.5 and 6. Ist XMLHttpRequest nicht vorhanden, wird versucht, die beste ActiveXObject-Variante zu verwenden
-if (typeof XMLHttpRequest === "undefined") {
-    XMLHttpRequest = function () {
-        try {
-            return new ActiveXObject("Msxml2.XMLHTTP.6.0");
-        }
-        catch (e) {
-        }
-        try {
-            return new ActiveXObject("Msxml2.XMLHTTP.3.0");
-        }
-        catch (e) {
-        }
-        try {
-            return new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        catch (e) {
-        }
-        throw new Error("Entschuldige, Dein Browser ist nicht kompatibel.");
-    };
-}
-
-var request = new XMLHttpRequest();
+let request = new XMLHttpRequest();
 
 function sendAJAXRequest(event) {
-    if (request.readyState === 0 || request.readyState === 4) {
+    if (request.readyState === XMLHttpRequest.UNSENT || request.readyState === XMLHttpRequest.DONE) {
         request.open("GET", "laender.php?index=" + event.target.selectedIndex, true);
-        request.onreadystatechange = handleResponse;
+        request.addEventListener("readystatechange", handleResponse);
         request.send(null);
     }
 }
 
 function handleResponse() {
-    if (request.readyState === 4) {
+    if (request.readyState === XMLHttpRequest.DONE) {
         document.getElementById("stadt").textContent = request.responseText;
     }
 }
 
-window.onload = function () {
-    var select = document.getElementById("bundesland");
-    select.onchange = sendAJAXRequest;
-};
+window.addEventListener("load", function () {
+    let select = document.getElementById("bundesland");
+    select.addEventListener("change", sendAJAXRequest);
+});
