2203973d28edd8bcced2b3aa79b44758fd6614d8
Wolfgang Hochleitner <wolfgang@hochleitner.cc>
2018-05-08 15:07:44 +0200

    Updated examples for lecture 7
    Added new custom Chuck Norris joke example to illustrate Composer.

---
+++ vl07/customchucknorris/index_phpcserrors.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Custom Chuck Norris Joke</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+<h1>Create your custom Chuck Norris Joke</h1>
+<form action="<?= $_SERVER["SCRIPT_NAME"] ?>" method="post">
+    <label for="firstName">Enter a first name:</label>
+    <input type="text" id="firstName" name="firstName"><br>
+    <label for="lastName">Enter a last name:</label>
+    <input type="text" id="lastName" name="lastName"><br>
+    <button type="submit">Roundhouse!</button>
+</form>
+</body>
+</html>
+
+<?php
+require "vendor/autoload.php";
+
+if (isset($_POST["firstName"]) && isset($_POST["lastName"])) {
+    $firstName = trim(
+        htmlspecialchars($_POST["firstName"], ENT_QUOTES | ENT_HTML5)
+    );
+    $lastName = trim(
+        htmlspecialchars($_POST["lastName"], ENT_QUOTES | ENT_HTML5)
+    );
+
+    $client = new GuzzleHttp\Client();
+
+    try {
+        $response = $client->request(
+            "GET",
+            "http://api.icndb.com/jokes/random",
+            [
+                "query" => [
+                    "firstName" => $firstName,
+                    "lastName"  => $lastName
+                ]
+            ]
+        );
+        $body = $response->getBody();
+        $data = json_decode($body);
+      echo "<p>" . $data->value->joke . "</p>";
+    } catch (\GuzzleHttp\Exception\GuzzleException $e) {
+        echo "<p>You are not ready for Chuck Norris!</p>";
+    }
+}
