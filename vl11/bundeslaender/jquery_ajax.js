7d1dfa14ffe53c8184e7ff41c5e40df43de98e93
Harrer Martin <P20137@fhooe.at>
2019-03-11 15:56:21 +0100

    Update jquery_ajax.js
    Fixed Typo

--- vl11/bundeslaender/jquery_ajax.js
+++ vl11/bundeslaender/jquery_ajax.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $("#bundesland").change(function (event) {
-        $.get("laender1.php", {index: event.target.selectedIndex}, function (data) {
+        $.get("laender.php", {index: event.target.selectedIndex}, function (data) {
             $("#stadt").text(data);
         }).fail(function () {
             $("#stadt").text("Es gab ein Problem mit der Anfrage der Hauptstadt.");
