0e740fc6a0bf54812e8461c0507f9223bf673acc
Wolfgang Hochleitner <wolfgang@hochleitner.cc>
2022-02-25 00:55:12 +0100

    Updated files for lecture 1
    Fixed a typo.

--- vl01/stringlength.php
+++ vl01/stringlength.php
@@ -1,6 +1,6 @@
 <?php
 $string = "Hällo";
 
-echo "<p>String, dessen länge abgefragt wird: $string</p>";
+echo "<p>String, dessen Länge abgefragt wird: $string</p>";
 echo "<p>Länge mit strlen: " . strlen($string) . "</p>"; // 6
 echo "<p>Länge mit mb_strlen: " . mb_strlen($string) . "</p>"; // 5
