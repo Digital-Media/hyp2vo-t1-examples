d45f960fe554ab21b2e6be43af51139302e51632
Wolfgang Hochleitner <wolfgang@hochleitner.cc>
2023-03-24 14:29:00 +0100

    Updated files for lecture 4
    Added new Chuck Norris example and removed the old one. ICNDb is dead, long live chucknorris.io.

---
+++ vl04/chucknorrisfact/index.php
@@ -0,0 +1,57 @@
+<?php
+
+require "vendor/autoload.php";
+
+$client = new GuzzleHttp\Client();
+
+$categoriesResponse = $client->request(
+    "GET",
+    "https://api.chucknorris.io/jokes/categories"
+);
+
+$categoriesBody = $categoriesResponse->getBody();
+$categories = json_decode($categoriesBody);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Random Chuck Norris Fact</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+<h1>Get a Random Chuck Norris Fact</h1>
+<form action="<?= $_SERVER["SCRIPT_NAME"] ?>" method="post">
+    <label for="category_selector">Select a category for a fact:</label>
+    <select name="category" id="category_selector">
+        <?php
+        foreach ($categories as $category) {
+            echo "<option value=\"$category\">$category</option>";
+        }
+        ?>
+    </select>
+    <button type="submit">Roundhouse!</button>
+</form>
+
+<?php
+if (isset($_POST["category"])) {
+    echo "<h2>Random fact from category " . $_POST["category"] . "</h2>";
+
+    $factResponse = $client->request(
+        "GET",
+        "https://api.chucknorris.io/jokes/random",
+        [
+            "query" => [
+                "category" => $_POST["category"]
+            ]
+        ]
+    );
+
+    $factBody = $factResponse->getBody();
+    $fact = json_decode($factBody);
+    echo "<p>" . $fact->value . "</p>";
+}
+?>
+
+</body>
+</html>
