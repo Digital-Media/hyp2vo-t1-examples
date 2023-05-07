d4f49892b2f266e79f425d80fadf1f856736a13f
Wolfgang Hochleitner <wolfgang@hochleitner.cc>
2023-05-07 22:48:54 +0200

    Updated examples for lecture 8
    Updated dates in date/time examples. Updated versions in i18n examples.

--- vl08/datumzeit/datetimeimmutable.php
+++ vl08/datumzeit/datetimeimmutable.php
@@ -1,7 +1,7 @@
 <?php
 
-$d1 = new DateTime("2022-04-25T15:30:00+02:00");
-$d2 = new DateTimeImmutable("2022-04-25T15:30:00+02:00");
+$d1 = new DateTime("2023-05-08T08:50:00+02:00");
+$d2 = new DateTimeImmutable("2023-05-08T08:50:00+02:00");
 $fortyFiveMinutes = new DateInterval("PT45M");
 
 $d1->add($fortyFiveMinutes);
--- vl08/datumzeit/datetimezone.php
+++ vl08/datumzeit/datetimezone.php
@@ -5,7 +5,7 @@ $timezone = new DateTimeZone("Europe/Vienna");
 /*$d1 = new DateTime("now", $timezone);
 echo "<p>It's now " . $d1->format("r") . "</p>";*/
 
-$d2 = new DateTime("2022-04-25T13:30:00+00:00");
+$d2 = new DateTime("2023-05-08T06:50:00+00:00");
 echo "<p>Original timestamp: " . $d2->format("r") . "</p>";
 $d2->setTimezone($timezone);
 echo "<p>Time zone corrected timestamp: " . $d2->format("r") . "</p>";
